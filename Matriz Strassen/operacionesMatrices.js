707a08944a1fc69aaa436dd360dc6cd6cfe0a007
gustavo garcia <dannyrr05@gmail.com>
2019-05-07 14:03:17 -0500

    armando Strassen

--- Matriz Strassen/operacionesMatrices.js
+++ Matriz Strassen/operacionesMatrices.js
@@ -1,5 +1,12 @@
 function comenzar() {
 
+
+    var matrizA = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]];
+    var matrizB = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]];
+
+    var matrizA1 = [[1, 2, 1, 2], [4, 4, 5, 6], [6, 5, 3, 7], [7, 8, 9, 8]];
+    var matrizB2 = [[2, 3, 4, 7], [6, 2, 6, 5], [5, 1, 7, 1], [4, 3, 8, 9]];
+
     var matrizPrueva = [
         [['a', 'b'], ['c', 'd']],
         [[1, 2], [3, 4]],
@@ -42,21 +49,13 @@ function comenzar() {
         [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []],
         [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
     ];
-    var ma = subDividir(matriz8x8);
-    
-    for (var i = 0; i < 4; i++) {
-        imprimirMatriz('mat sub: ', ma[i]);
-        console.log('-------------------------------');
-    }
+    var ma = restarMatrices(matrizA1, matrizB2);
+    Strassenn(matrizA1, matrizB2);
 
 }
 
-function imprimirMatriz(texto, matriz) {
-    for (var i = 0; i < matriz.length; i++) {
-        console.log(texto + matriz[i]);
-    }
-}
 
+//---------------------Operaciones internas de Strassen------------------
 function subDividir(matriz) {
     var matrizSubdividida = [];
     var numeroSubMatrices = 4;
@@ -86,3 +85,153 @@ function subDividir(matriz) {
     }
     return matrizSubdividida;
 }
+
+function calcularPotenciaDe2AlaN(matriz) {
+    var potenciaDe2AlaNActual;
+
+
+    var numeroFilas = matriz.length;
+    var numeroColumnas = matriz[0].length;
+
+    var dimensionMayor = Math.max(numeroFilas, numeroColumnas);
+    potenciaDe2AlaNActual = Math.ceil(Math.log(dimensionMayor) / Math.log(2));
+
+    return potenciaDe2AlaNActual;
+}
+function calcularNuevaPotenciaDe2AlaN(potenciaDe2AlaNActual) {
+
+    var nuevoExponente2AlaN = potenciaDe2AlaNActual;
+
+    while (nuevoExponente2AlaN % Math.pow(2, potenciaDe2AlaNActual)) {
+        nuevoExponente2AlaN += 1;
+    }
+    return nuevoExponente2AlaN;
+}
+
+//------------
+
+function completarConCeros(matriz) {
+
+    var nuevaMatriz = matriz;
+
+    var numeroFilas = matriz.length;
+    var numeroColumnas = matriz[0].length;
+
+    var nuevaDimension = calcularNuevaPotenciaDe2AlaN(calcularPotenciaDe2AlaN(matriz));
+
+    nuevaMatriz = completarConCerosFilas(nuevaMatriz, nuevaDimension, numeroFilas, numeroColumnas);
+    nuevaMatriz = completarConCerosColumnas(nuevaMatriz, nuevaDimension, numeroFilas);
+
+    return nuevaMatriz;
+}
+
+function completarConCerosFilas(matriz, nuevaDimension, numeroFilas, numeroColumnas) {
+
+    var matrizFilasCompletadas = matriz;
+    for (let i = 0; i < numeroFilas; i++) {
+        for (let j = numeroColumnas; j < nuevaDimension; j++) {
+            matrizFilasCompletadas[i].push(0);
+        }
+    }
+    return matrizFilasCompletadas;
+}
+
+function completarConCerosColumnas(matriz, nuevaDimension, numeroFilas) {
+    var matrizColumnasCompletadas = matriz;
+    for (let i = numeroFilas; i < nuevaDimension; i++) {
+        matrizColumnasCompletadas.push([0]);
+        for (let j = 0; j < nuevaDimension; j++) {
+            matrizColumnasCompletadas[i].push(0);
+        }
+
+    }
+    return matrizColumnasCompletadas;
+}
+
+//---------------- Multiplicar por Strassen-------------
+
+function Strassenn(matrizACompletada, matrizBCompletada) {
+
+    if (verificarSiSePuedenMultiplicar(matrizAA, matrizBA)) {
+
+        var matrizACompletada = completarConCeros(matrizA)*/;
+        var matrizBCompletada = completarConCeros(matrizB)*/;
+
+
+        //A= matrizA Subdividida
+        var A = subDividir(matrizACompletada);
+        //B= matrizB Subdividida
+        var B = subDividir(matrizBCompletada);
+
+        //A11= matrizA[0] ; A12= matrizA[1] ; A21= matrizA[2] ; A22= matrizA[3]
+        //B11= matrizB[0] ; B12= matrizB[1] ; B21= matrizB[2] ; B22= matrizB[3]
+
+        var m1 = Strassenn(sumarMatrices(A[0], A[3]), sumarMatrices(B[0], B[3]));
+        var m2 = Strassenn(sumarMatrices(A[2], A[3]), B[0]);
+        var m3 = Strassenn(A[0], restarMatrices(B[1], B[3]));
+        var m4 = Strassenn(A[3], restarMatrices(B[2], B[0]));
+        var m5 = Strassenn(sumarMatrices(A[0], A[1]), B[3]);
+        var m6 = Strassenn(restarMatrices(A[2], A[0]), sumarMatrices(B[0], B[1]));
+        var m7 = Strassenn(restarMatrices(A[1], A[3]), sumarMatrices(B[2], B[3]));
+
+        var c11 = sumarMatrices(restarMatrices(sumarMatrices(m1, m4), m5), m7);
+        var c12 = sumarMatrices(m3, m5);
+        var c21 = sumarMatrices(m2, m4);
+        var c22 = sumarMatrices(sumarMatrices(restarMatrices(m1, m2), m3), m6);
+
+
+    } else {
+        alert('No se pueden multiplicar las matrices; filas de A[i]: ' + matrizACompletada.length + ' diferentes de columnas de B[j]: ' + matrizBCompletada[0].length);
+    }
+
+}
+
+
+//-----------------------Operaciones Varias-------------------
+
+function imprimirMatriz(texto, matriz) {
+    for (var i = 0; i < matriz.length; i++) {
+        console.log(texto + matriz[i]);
+    }
+}
+
+function sumarMatrices(matriz1, matriz2) {
+    var matrizSumada = [];
+
+    for (var i = 0; i < matriz1.length; i++) {
+        matrizSumada.push([]);
+        for (var j = 0; j < matriz1[i].length; j++) {
+            matrizSumada[i].push(matriz1[i][j] + matriz2[i][j]);
+        }
+    }
+
+    return matrizSumada;
+}
+
+function restarMatrices(matriz1, matriz2) {
+    var matrizRestada = [];
+
+    for (var i = 0; i < matriz1.length; i++) {
+        matrizRestada.push([]);
+        for (var j = 0; j < matriz1[i].length; j++) {
+            matrizRestada[i].push(matriz1[i][j] - matriz2[i][j]);
+        }
+    }
+
+    return matrizRestada;
+}
+
+function verificarSiSePuedenMultiplicar(matriz1, matriz2) {
+    var sePuedeMultiplicar;
+
+    if (matriz1[0].length == matriz2.length) {
+        sePuedeMultiplicar = true;
+    } else {
+        sePuedeMultiplicar = false;
+    }
+
+    return sePuedeMultiplicar;
+
+}
+
+
