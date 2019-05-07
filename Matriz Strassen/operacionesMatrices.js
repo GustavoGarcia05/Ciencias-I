cbd3839a2fe826658e62672a1c444578419eac09
gustavo garcia <dannyrr05@gmail.com>
2019-05-07 09:11:46 -0500

    Straseen con subdividir completado

--- Matriz Strassen/operacionesMatrices.js
+++ Matriz Strassen/operacionesMatrices.js
@@ -7,6 +7,13 @@ function comenzar() {
         [[11, 22], [33, 44]]
     ];
 
+    var matrizA1 = [
+        [1, 2, 1, 2],
+        [4, 4, 5, 6],
+        [6, 5, 3, 7],
+        [7, 8, 9, 8]
+    ];
+
     var matriz8x8 = [
         [1, 2, 3, 4, 5, 6, 7, 8],
         [11, 12, 13, 14, 15, 16, 17, 18],
@@ -17,7 +24,6 @@ function comenzar() {
         [61, 62, 63, 64, 65, 66, 67, 68],
         [71, 72, 73, 74, 75, 76, 77, 78]
     ];
-
     var matriz16x16 = [
         [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []],
         [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []],
@@ -36,7 +42,13 @@ function comenzar() {
         [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []],
         [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
     ];
-    imprimirMatriz('matriz: ', subDividir(matriz8x8));
+    var ma = subDividir(matriz8x8);
+    
+    for (var i = 0; i < 4; i++) {
+        imprimirMatriz('mat sub: ', ma[i]);
+        console.log('-------------------------------');
+    }
+
 }
 
 function imprimirMatriz(texto, matriz) {
@@ -50,31 +62,27 @@ function subDividir(matriz) {
     var numeroSubMatrices = 4;
     var tamanioSubDivisiones = matriz.length / 2;
 
-    var coordnenadaX = 0;
-    var coordnenadaY = 0;
+    var coordenadaX = 0;
+    var coordenadaY = 0;
 
     for (var k = 0; k < numeroSubMatrices; k++) {
+        var matrizAuxiliar = [];
         if (k < 2) {
-            coordnenadaX = 0;
-            coordnenadaY = k * (tamanioSubDivisiones - 1);
+            coordenadaX = 0;
+            coordenadaY = k * (tamanioSubDivisiones);
         } else {
-            coordnenadaX = tamanioSubDivisiones - 1;
+            coordenadaX = tamanioSubDivisiones;
             var kAux = k - (numeroSubMatrices / 2);
-            coordnenadaY = kAux * (tamanioSubDivisiones - 1);
+            coordenadaY = kAux * (tamanioSubDivisiones);
         }
-
-        for (var i = coordnenadaX; i < tamanioSubDivisiones + coordnenadaX; i++) {
-            for (var j = coordnenadaY; j < tamanioSubDivisiones + coordnenadaY; j++) {
-
+        //console.log('x,y: ' + coordenadaX + ',' + coordenadaY);
+        for (var i = coordenadaX; i < tamanioSubDivisiones + coordenadaX; i++) {
+            matrizAuxiliar.push([]);
+            for (var j = coordenadaY; j < tamanioSubDivisiones + coordenadaY; j++) {
+                matrizAuxiliar[i - coordenadaX].push(matriz[i][j]);
             }
         }
-        console.log('x,y: ' + coordnenadaX + ',' + coordnenadaY);
-
-
+        matrizSubdividida.push(matrizAuxiliar);
     }
-
-
-
-
     return matrizSubdividida;
 }
