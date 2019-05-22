ac43663dcdeaa5231a9c166717e8868ddb46761d
Gustavo Garcia <dannyrr05@gmail.com>
2019-05-21 23:23:52 -0500

    Listas sin parte grafica (sin graficar)

--- Listas/src/Launcher.java
+++ Listas/src/Launcher.java
@@ -14,9 +14,23 @@ public class Launcher {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Nodo p= new Nodo(5);
-        System.out.println(p.info);
+
+        //Nodo p= new Nodo(5);
+        Lista list = new Lista();
+        list.insertar(10);
+        list.insertar(-2);
+        list.insertar(-5);
+        list.insertar(22);
+        list.insertar(15);
+        list.insertar(-32);
+        list.insertar( -2);
+        list.insertar( -32);
+
+
+        list.imprimir();
+        
+        list.dibujar();
+ 
     }
-    
+
 }
--- Listas/src/Lista.java
+++ Listas/src/Lista.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,19 +11,22 @@
  */
 public class Lista {
 
-    Nodo cabeza;
+    private Nodo cabeza;
 
     public Lista() {
         cabeza = null;
     }
 
-    Nodo insertar(Nodo p, int d) {
+    Nodo insertar(int d) {
+        if(buscar(d)){
+            System.out.println("ya esta el numero");
+            return null; 
+        }
         Nodo q;
         Nodo s;
 
         q = null;
-        s = p;
-
+        s = cabeza;
         while (s != null && d > s.info) {
             if (s.info == d) {
                 return null;
@@ -33,27 +37,76 @@ public class Lista {
         Nodo n = new Nodo(d);
 
         if (q == null) {
-            n.sig = p;
-            p = n;
-        }else if(s==null){
-            q.sig=n;
-        }else{
-            n.sig=s;
-            q.sig=n;
+            n.sig = cabeza;
+            cabeza = n;
+        } else if (s == null) {
+            q.sig = n;
+        } else {
+            n.sig = s;
+            q.sig = n;
         }
-        return p;
+        return cabeza;
     }
 
-    boolean retirar() {
-        return false;
+    boolean retirar(int d) {
+        boolean esta;
+        esta = buscar(d);
+
+        if (esta == false) {
+            return false;
+        }
+
+        Nodo aux = null;
+        Nodo q = null;
+        Nodo s = cabeza;
+
+        while (s != null && s.info <= d) {
+            aux = q;
+            q = s;
+            s = s.sig;
+        }
+        System.out.println("este es aux " + aux.info);
+        System.out.println("este es q " + q.info);
+
+        if (s != null) {
+            q.info = s.info;
+            q.sig = s.sig;
+        } else {
+            
+            aux.sig=null;
+            q=aux;
+        }
+        return true;
     }
 
     void imprimir() {
+        Nodo q = cabeza;
 
+        while (q != null) {
+            System.out.println(q.info);
+            q = q.sig;
+        }
     }
 
     boolean buscar(int d) {
-        return false;
+        Nodo q = cabeza;
+        while (q != null && q.info < d) {
+            q = q.sig;
+        }
+        if (q != null && q.info == d) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    
+    void dibujar(){
+        new Ventana(this);
+    }
+
+    public Nodo getCabeza() {
+        return cabeza;
     }
 
 }
