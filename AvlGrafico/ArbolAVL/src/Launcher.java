f9b0a33e3c763c428ac20bc2fdc7b8c240bc6477
gustavo garcia <dannyrr05@gmail.com>
2019-07-10 07:27:11 -0500

    avl grafico incompleto

--- AvlGrafico/ArbolAVL/src/Launcher.java
+++ AvlGrafico/ArbolAVL/src/Launcher.java
@@ -1,4 +1,5 @@
 
+import modelo.ArbolPrueva;
 import vista.Ventana;
 
 /*
@@ -6,7 +7,6 @@ import vista.Ventana;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Gustavo
@@ -18,7 +18,33 @@ public class Launcher {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        new Ventana();
+        //new Ventana();
+        ArbolPrueva ap = new ArbolPrueva();
+        ap.insertar(10);
+        ap.insertar(5);
+        //ap.insertar(15);
+        ap.insertar(3);
+        //ap.insertar(1);
+        //ap.insertar(7);
+        //ap.insertar(13);
+        //ap.insertar(17);
+
+        /*System.out.println("raiz: "+ap.getRaiz().dato);
+        System.out.println("izq: "+ap.getRaiz().izq.dato);
+        System.out.println("der: "+ap.getRaiz().der.dato);
+        System.out.println("izq.izq: "+ap.getRaiz().izq.izq.dato);
+        System.out.println("izq.izq: "+ap.getRaiz().izq.der.dato);
+        System.out.println("der.izq: "+ap.getRaiz().der.izq.dato);
+        System.out.println("der.izq: "+ap.getRaiz().der.der.dato);*/
+ /*
+        System.out.println("balance: "+ ap.factorBalance(ap.getRaiz()));
+        System.out.println("balance: "+ ap.factorBalance(ap.getRaiz().der));
+         */
+        System.out.println("izq" + ap.getRaiz().dato);
+        ap.rotacionSimpleIzquierda(ap.getRaiz().izq,ap.getRaiz().izq.izq);
+        System.out.println("izq" + ap.getRaiz().dato);
+        //System.out.println("izq" + ap.getRaiz().izq.dato);*/
+
     }
-    
+
 }
--- AvlGrafico/ArbolAVL/src/modelo/ArbolPrueva.java
+++ AvlGrafico/ArbolAVL/src/modelo/ArbolPrueva.java
@@ -19,6 +19,86 @@ public class ArbolPrueva {
         raiz = null;
     }
 
+    public void insertar(int dato) {
+        Nodo nuevo = new Nodo(dato);
+        Nodo q = raiz;
+        Nodo p = null;
+
+        if (raiz == null) {
+            raiz = nuevo;
+            return;
+
+        } else {
+            while (q != null) {
+                p = q;
+                if (dato == q.dato) {
+                    return;
+                }
+                if (dato < q.dato) {
+                    q = q.izq;
+                    continue;
+                }
+                if (dato > q.dato) {
+                    q = q.der;
+                    continue;
+                }
+            }
+            if (dato < p.dato) {
+                p.izq = nuevo;
+                nuevo.padre = p;
+            }
+            if (dato > p.dato) {
+                p.der = nuevo;
+                nuevo.padre = p;
+            }
+            p.balance = factorBalance(p);
+        }
+    }
+
+    public int factorBalance(Nodo n) {
+        if (n == null) {
+            return 99;
+        }
+        return altura(n.izq) - altura(n.der);
+    }
+
+    public int altura(Nodo n) {
+        if (n == null) {
+            return 0;
+        } else {
+            return 1 + (Math.max(altura(n.izq), altura(n.der)));
+        }
+
+    }
+
+    public void rotarIzq(Nodo n) {
+        Nodo temp = n;
+        
+        n = temp.der;
+        temp.der = n.izq;
+
+        n.izq = temp;
+    }
+
+    public void rotarDer(Nodo n) {
+        System.out.println(" rot simple der");
+    }
+
+    public void rotarIzq_Der(Nodo n) {
+        System.out.println(" rot simple izq der");
+    }
+
+    public void rotarDer_Izq(Nodo n) {
+        System.out.println(" rot simple der izq");
+    }
+
+    public Nodo getRaiz() {
+        return raiz;
+    }
+
+}
+
+/*
     public void insertar(int d) {
         Nodo nuevo = new Nodo(d);
         if (raiz == null) {
@@ -37,11 +117,35 @@ public class ArbolPrueva {
                 if (d < p.dato) {
                     q = p;
                     p = p.izq;
-                }
-                if(d>p.dato){
+                }else if(d>p.dato){
                     q=p;
                     p=p.der;
                 }
+            }
+            //insercion del nodo 
+            if(d<q.dato){
+                q.izq=nuevo;
+                nuevo.padre=q;
+            }
+            if(d>q.dato){
+                q.der=nuevo;
+                nuevo.padre=q;
+            }
+            //Equilibrar el arbol
+            if(q==raiz){
+                System.out.println("en proceso");
+            }
+            if(q.padre.balance==-2 &&q.balance==-1 ){
+                rotacionIzq();
+            }
+            if(q.padre.balance==2 &&q.balance==1 ){
+                rotacionDer();
+            }
+            if(q.padre.balance==-2 &&q.balance==1 ){
+                rotacionIzq_Der();
+            }
+            if(q.padre.balance==2 &&q.balance==-1 ){
+                rotacionDer_izq();
             }
             
             
@@ -51,12 +155,20 @@ public class ArbolPrueva {
     
     public int calcularBalance(Nodo acutual){
         int balance=0;
-        
         if(acutual.izq==null){
-            
+            return balance=3;
         }
+        balance=altura(acutual.izq)-altura(acutual.der);
         
         return balance;
+    }
+    public int altura(Nodo c){
+        int altura=0;
+        if(c==null){
+            return altura;
+        }
+        altura=Math.max(altura(c.izq), altura(c.der))+1;
+        return altura;
     }
 
     public void rotacionIzq() {
@@ -73,6 +185,4 @@ public class ArbolPrueva {
 
     public void rotacionDer_izq() {
 
-    }
-
-}
+    }*/
