a51c3f3aa880cece5ca1b2ab4301de657c6e097d
Gustavo Garcia <dannyrr05@gmail.com>
2019-07-14 16:21:37 -0500

    Arbol avl con insercion y retiro

--- ArbolesBinariosAVLyRN/src/Controlador/Control.java
+++ ArbolesBinariosAVLyRN/src/Controlador/Control.java
@@ -9,38 +9,57 @@ import Vista.Ventana;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.Integer;
 
 /**
  *
  * @author Gustavo
  */
-public class Control implements ActionListener{
+public class Control implements ActionListener {
+
     private Ventana v;
 
     public Control(Ventana v) {
         this.v = v;
     }
-    
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==v.getInsertar()){
-            
+        if (e.getSource() == v.getInsertar()) {
+            Object numero=Integer.parseInt(v.getCajaInsertar().getText());
+            if(!numero.equals(Integer)){
+                return;
+            }
             v.getArbolAVL().insertar(Integer.parseInt(v.getCajaInsertar().getText()));
             v.getArbolRN().insertar(Integer.parseInt(v.getCajaInsertar().getText()));
+
             v.getCajaInsertar().setText("");
+
+            dibujarAVL();
+            dibujarRN();
+
+        }
+        
+        if(e.getSource()== v.getRetirar()){
+            v.getArbolAVL().retirar(Integer.parseInt(v.getCajaRetirar().getText()));
             
-            v.getDibujoAVL().setObjArbol(v.getArbolAVL());
-            v.getPanelAVL().setViewportView(v.getDibujoAVL());
-            
-            v.getDibujoRN().setObjArbol(v.getArbolRN());
-            v.getPanelRN().setViewportView(v.getDibujoRN());
-            
-            
-            //v.getPanelRN().add(v.getDibujoAVL());
-            //v.getPanelAVL().setViewportView(v.getDibujoAVL());
-            //v.getPanelRN().setViewportView(v.getDibujoAVL());
+            v.getCajaRetirar().setText("");
+            dibujarAVL();
         }
     }
-    
+
+    private void dibujarAVL() {
+
+        v.getDibujoAVL().setObjArbol(v.getArbolAVL());
+        v.getPanelAVL().setViewportView(v.getDibujoAVL());
+
+    }
+
+    private void dibujarRN() {
+
+        v.getDibujoRN().setObjArbol(v.getArbolRN());
+        v.getPanelRN().setViewportView(v.getDibujoRN());
+
+    }
+
 }
