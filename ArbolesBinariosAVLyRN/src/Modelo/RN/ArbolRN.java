48b4d7b4b1c8da82a7ea73f8bce77979ee895590
Brayan Lopez <>
2019-07-15 00:39:09 -0500

    implementado metodo eliminar, insetar y los metodos que necesitan de ayuda en el arbol Rojinegro. Metodo tomado de la pagina PorComputador.com. ese metodo tiene errores. Esta imposible de leer o entender.

--- ArbolesBinariosAVLyRN/src/Modelo/RN/ArbolRN.java
+++ ArbolesBinariosAVLyRN/src/Modelo/RN/ArbolRN.java
@@ -76,9 +76,10 @@ public class ArbolRN {
      */
     public NodoRN insertar(int v) {
         NodoRN gf, g, p, x;
-        if (raiz == null) {
+
+        /*if (raiz == null) {
             raiz = new NodoRN(v);
-        }
+        }*/
         x = raiz;
         p = x;
         g = x;
@@ -112,8 +113,183 @@ public class ArbolRN {
         x = dividir(v, gf, g, p, x);
         return x;
     }
-    
-    
+
+    boolean colorRojo(NodoRN raiz) {
+        return raiz != z && raiz.color == rojo;
+    }
+
+    NodoRN simpleRotacion(NodoRN raiz, int dir) {
+        NodoRN temp;
+        if (dir == 1) {
+            System.out.println("simple rotacion a la der.");
+            temp = raiz.izq;
+            raiz.izq = temp.der;
+            temp.der = raiz;
+        } else {
+            System.out.println("simple rotacion a la izq");
+            temp = raiz.der;
+            raiz.der = temp.izq;
+            temp.izq = raiz;
+        }
+        raiz.color = rojo;
+        temp.color = negro;
+        return temp;
+    }
+
+    NodoRN doblerotacion(NodoRN raiz, int dir) {
+        if (dir == 1) {
+            raiz.izq = simpleRotacion(raiz.izq, 0);
+        } else {
+            raiz.der = simpleRotacion(raiz.der, 1);
+        }
+        return simpleRotacion(raiz, dir);
+    }
+
+    /**
+     * elimina un dato en el arbol
+     *
+     * @return el dato que se ha eliminado.
+     */
+    public int eliminar(NodoRN arbol, int llave) {
+        if (arbol.der != z) {
+            NodoRN cabeza = new NodoRN(0);
+            cabeza.color = negro;
+            cabeza.izq = z;
+            cabeza.der = z;
+            NodoRN temp, q, p, g;
+            NodoRN t, f = z;
+            int dir = 1;
+
+            q = cabeza;
+            g = p = z;
+            q.der = arbol.der;
+
+            t = q.der;
+
+            while (t != z) {
+                int anterior = dir;
+                NodoRN w, v;
+
+                g = p;
+                p = q;
+
+                if (anterior == 0) {
+                    v = p.der;
+                } else {
+                    v = p.izq;
+                }
+                if (dir == 0) {
+                    q = q.izq;
+                } else {
+                    q = q.der;
+                }
+                //actualiza dir
+                if (q.llave < llave) {
+                    dir = 1;
+                } else {
+                    dir = 0;
+                }
+                if (q.llave == llave) {
+                    f = q;
+                }
+                if (dir == 0) {
+                    t = q.izq;
+                    w = q.der;
+                } else {
+                    t = q.der;
+                    w = q.izq;
+                }
+                if (!colorRojo(q) && !colorRojo(t)) {
+                    if (colorRojo(w)) {
+                        temp = simpleRotacion(q, dir);
+                        if (anterior == 0) {
+                            p.izq = temp;
+                        } else {
+                            p.der = temp;
+                        }
+                        p = temp;
+                    } else if (!colorRojo(w)) {
+                        NodoRN s = v;
+                        NodoRN r1, v1;
+                        if (anterior == 0) {
+                            r1 = s.izq;
+                            v1 = s.der;
+                        } else {
+                            r1 = s.der;
+                            v1 = s.izq;
+                        }
+                        if (s != z) {
+                            if (!colorRojo(v1) && !colorRojo(r1)) {
+                                p.color = negro;
+                                s.color = rojo;
+                                q.color = rojo;
+                            } else {
+                                int dir2;
+                                NodoRN r2;
+                                if (g.der == p) {
+                                    dir2 = 1;
+                                } else {
+                                    dir2 = 0;
+                                }
+                                if (dir2 == 0) {
+                                    r2 = g.izq;
+                                } else {
+                                    r2 = g.der;
+                                }
+                                if (colorRojo(r1)) {
+                                    if (dir2 == 0) {
+                                        System.out.println("doble rotacion a la izq");
+                                        g.izq = doblerotacion(p, anterior);
+                                    } else {
+                                        System.out.println("doble rotacion a la der");
+                                        g.der = doblerotacion(p, anterior);
+                                    }
+                                } else if (colorRojo(v1)) {
+                                    if (dir2 == 0) {
+                                        g.izq = simpleRotacion(p, anterior);
+                                    } else {
+                                        g.der = simpleRotacion(p, anterior);
+                                    }
+                                }
+                                if (dir2 == 0) {
+                                    r2 = g.izq;
+                                } else {
+                                    r2 = g.der;
+                                }
+                                q.color = r2.color = rojo;
+                                r2.izq.color = negro;
+                                r2.der.color = negro;
+                            }
+                        }///if(s!=z)
+                    }///if(!colorRojo(w))
+                }///if(!colorRojo(q)&&!colorRojo(t))
+            }///while(terminar!=z)
+
+            if (f != z) {
+                NodoRN h = null;
+                f.llave = q.llave;
+                if (q.izq == z) {
+                    h = q.der;
+                } else {
+                    h = q.izq;
+                }
+                if (p.der == q) {
+                    p.der = h;
+                } else {
+                    p.izq = h;
+                }
+                q = null;
+            } else {
+                System.out.println("!no existe la llave");
+            }
+            arbol.der = cabeza.der;
+            cabeza = null;
+            if (arbol.der != z) {
+                arbol.der.color = negro;
+            }
+        }
+        return 1;
+    }
 
     /**
      * busca un nodo en un arbol
